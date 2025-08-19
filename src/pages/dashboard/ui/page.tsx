f4b854ea2308b643aa7b9e1bb05cfaefdad9544f
nikita <n.shiyan@thewhite.ru>
2025-08-19 23:45:02 +1000

    added responsive design

--- src/pages/dashboard/ui/page.tsx
+++ src/pages/dashboard/ui/page.tsx
@@ -1,16 +1,42 @@
-import {Box} from "@mui/material";
+import {Box, Drawer, IconButton} from "@mui/material";
 import {ChatMenu} from "@/entities/chatsMenu";
 import {Outlet} from "react-router-dom";
 import {Toaster} from "react-hot-toast";
+import MenuIcon from '@mui/icons-material/Menu';
+import s from './dashboardPage.module.scss'
+import {useState} from "react";
 
-export const DashboardPage = () =>
-    (
+export const DashboardPage = () => {
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+    const handleOpen = () => setMobileMenuOpen(true);
+    const handleClose = () => setMobileMenuOpen(false);
+
+    return (
         <Box
-            display={'flex'}
-            height={'100%'}
+            className={s['page']}
         >
             <Toaster/>
-            <ChatMenu/>
+            <Box
+                className={s['chat-menu-desktop']}
+            >
+                <ChatMenu/>
+            </Box>
+            <IconButton
+                className={s['mobile-menu-button']}
+                onClick={handleOpen}
+            >
+                <MenuIcon/>
+            </IconButton>
+            <Drawer
+                anchor="left"
+                open={mobileMenuOpen}
+                onClose={handleClose}
+                sx={{display: {xs: "block", md: "none"}}}
+            >
+                <ChatMenu/>
+            </Drawer>
             <Outlet/>
         </Box>
-    )
+    );
+}
