c49fa8f9c6383daed5ff49dd02073faaba1be5d0
nikita <n.shiyan@thewhite.ru>
2025-08-19 14:07:45 +1000

    refactor

--- src/entities/templateEdit/ui/templateEdit.tsx
+++ src/entities/templateEdit/ui/templateEdit.tsx
@@ -1,12 +1,8 @@
 import {TemplateForm} from "@/shared/ui/templateForm/templateForm.tsx";
-import {Box} from "@mui/material";
 
-export const TemplateEdit = () => {
-    return (
-        <Box>
-            <TemplateForm
-                isEdit={true}
-            />
-        </Box>
+export const TemplateEdit = () =>
+    (
+        <TemplateForm
+            isEdit={true}
+        />
     )
-}
